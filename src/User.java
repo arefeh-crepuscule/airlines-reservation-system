2f8a33e668f0e15e4dc311d3cdfb56668a29f484
arefeh-crepuscule <arefeh.za1382@gmail.com>
2023-04-11 22:58:47 +0430

    functions : checkPass , changePass, inputError

--- src/User.java
+++ src/User.java
@@ -22,6 +22,10 @@ public class User {
         System.out.println("   <6> Add charge");
         System.out.println("   <0> Sign out");
     }
+
+    /**
+     * give the extra charge that user want to charge and call the update function
+     */
     public void addCharge (){
         System.out.printf("Your current charge :\t%10d\n", charge);
         System.out.print("How much do you want to charge?\t");
@@ -31,9 +35,44 @@ public class User {
         System.out.printf("Your charge has been updated :\t%10s",charge);
     }
 
+    /**
+     * updated private charge with extra parameter
+      * @param extraCharge the variable for add or subtract charge variable
+     */
     private void chargeUpdate(int extraCharge) {
         charge += extraCharge;
     }
 
+    /**
+     * check input pass that be as same as saved pass in password variable
+     * @param inputPass new pass that we want to check it
+     * @return true or false mean new pass is same or not
+     */
+    public boolean checkPass(String inputPass){
+        if ( passWord.equals(inputPass))
+            return true;
+        return false;
+    }
 
+    /**
+     * change user password with check old password , get new password and update pass word
+      */
+    public void changePass(){
+        System.out.print("enter your current password :\t");
+        String temp = scanner.nextLine();
+        if(checkPass(temp)){
+            passWord = temp;
+            System.out.printf(cls+"\nYour password has been updated");
+        }else {
+            inputError();
+        }
+    }
+
+    /**
+     * this function print error input means user input is wrong and need try again
+      */
+    private void inputError() {
+        System.out.println(cls);
+        System.out.print("Incorrect input , try again!! \n\n");
+    }
 }
